3a356f085fe1f54d768ead747bcfcef6e825339f
a <128429841+VictorTI26@users.noreply.github.com>
2023-10-02 21:51:54 -0300

    1

---
+++ VITE/src/App.jsx
@@ -0,0 +1,10 @@
+import Banner from "./components/Banner";
+import Formulario from "./components/Formulario";
+
+const App = () => {
+  return <div>
+    <Banner />
+  </div>; 
+};
+
+export default App;
